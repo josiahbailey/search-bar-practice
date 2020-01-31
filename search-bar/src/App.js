bb26e12c1a22f1bfc434996b92e4398ea46931ee
Josiah <reikiryo@yahoo.com>
2020-01-30 19:15:32 -0500

    file structure in place, axios set up with breaking bad api

--- search-bar/src/App.js
+++ search-bar/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Container from './components/container'
 
 function App() {
   return (
     <div className="App">
-    
+		<Container />
     </div>
   );
 }
---
+++ search-bar/src/components/container.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Search from './search'
+import Card from './card'
+
+const Container = () => {
+    return (  
+        <div>
+        Container
+            <Search/>
+            <Card/>
+        </div>
+    );
+}
+ 
+export default Container;
