381bb7eca4153640b6ccd7fd2a65db4800eaa164
Josiah <reikiryo@yahoo.com>
2020-01-30 19:45:37 -0500

    ads character cards

--- search-bar/src/components/container.js
+++ search-bar/src/components/container.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './search'
 import Card from './card'
+import axios from 'axios'
 
 const Container = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get(`https://www.breakingbadapi.com/api/characters`)
+        .then(res => {
+            setData(res.data)
+            console.log(res)
+        })
+        .catch(err => {
+            console.log("ERROR", err)
+        })
+    }, [])
     return (  
         <div>
         Container
             <Search/>
-            <Card/>
+            {data.map(char => (
+                <Card key={char.img} char={char}/>
+            ))}
         </div>
     );
 }
